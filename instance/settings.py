52606cb05e0a6e19d1b98752098c9aee5c073168
Kiran Jonnalagadda <jace@pobox.com>
2012-03-08 22:29:35 +0530

    Put settings.py in the repo since there are no secrets in it.

---
+++ instance/settings.py
@@ -0,0 +1,8 @@
+# Check this into the repo now
+
+#: Google Analytics tracking code
+GA_CODE = 'UA-19123154-19'
+#: Typekit font code, from the embed URL: http://use.typekit.com/(code).js
+TYPEKIT_CODE = 'wxg4owb'
+#: Timezone for displayed datetimes
+TIMEZONE = 'Asia/Calcutta'
