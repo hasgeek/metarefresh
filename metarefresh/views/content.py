b911bbc51823f46da1f3c4f631ff0f12747091eb
Kiran Jonnalagadda <jace@pobox.com>
2012-03-30 20:50:37 +0530

    Changed URL and email address for takecharge contest.

--- metarefresh/views/content.py
+++ metarefresh/views/content.py
@@ -1,13 +1,18 @@
 from metarefresh import app
-from flask import render_template
+from flask import render_template, redirect, url_for
+
 
 @app.route('/')
 def index():
     return render_template('index.html', active=1)
 
 
-@app.route('/take-charge')
+@app.route('/takecharge')
 def contest():
     return render_template('contest.html', active=1)
 
 
+@app.route('/take-charge')
+@app.route('/take-charge.html')
+def contest_old_url():
+    return redirect(url_for('contest'), code=301)
