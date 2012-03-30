d9c5ac3d3282b262a17c451bf71884dbc0f43d36
Prateek Rungta <prateekrungta@gmail.com>
2012-03-30 15:02:49 +0530

    Added the contest page

--- metarefresh/views/content.py
+++ metarefresh/views/content.py
@@ -5,3 +5,9 @@ from flask import render_template
 def index():
     return render_template('index.html', active=1)
 
+
+@app.route('/take-charge')
+def contest():
+    return render_template('contest.html', active=1)
+
+
