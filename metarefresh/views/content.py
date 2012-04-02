7e5d7cf8b453d591f4f560c3de871c84f35ae0f8
Kiran Jonnalagadda <jace@pobox.com>
2012-04-02 22:55:04 +0530

    Move the website to /2012.

--- metarefresh/views/content.py
+++ metarefresh/views/content.py
@@ -4,15 +4,21 @@ from flask import render_template, redirect, url_for
 
 @app.route('/')
 def index():
+    return redirect(url_for('index2012'), code=302)
+
+
+@app.route('/2012/')
+def index2012():
     return render_template('index.html', active=1)
 
 
-@app.route('/takecharge')
+@app.route('/2012/takecharge')
 def contest():
     return render_template('contest.html', active=1)
 
 
 @app.route('/take-charge')
 @app.route('/take-charge.html')
+@app.route('/takecharge')
 def contest_old_url():
     return redirect(url_for('contest'), code=301)
