5c9599bdf9c8d905738f0fb329be764d973a9d9e
Kiran Jonnalagadda <jace@pobox.com>
2012-04-05 00:41:40 +0530

    Set environment before importing all.

--- runserver.py
+++ runserver.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
+from os import environ
+environ['METAREFRESH_ENV'] = 'development'
+
 from metarefresh import app
 from metarefresh.models import db
-from os import environ
 
-environ['METAREFRESH_ENV'] = 'development'
-#app.config['ASSETS_DEBUG'] = True
 db.create_all()
 app.run('0.0.0.0', 6200, debug=True)
